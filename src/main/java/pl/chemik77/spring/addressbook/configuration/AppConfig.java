1c96ce5e9d61c4b4812a9698bdcef671d2cae53a
chemik77 <ania.kawala@gmail.com>
2018-01-13 22:42:07 +0100

    +/- Change persons list view

--- src/main/java/pl/chemik77/spring/addressbook/configuration/AppConfig.java
+++ src/main/java/pl/chemik77/spring/addressbook/configuration/AppConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -28,13 +27,6 @@ public class AppConfig implements WebMvcConfigurer {
 
 	public void setApplicationContext(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		WebMvcConfigurer.super.addResourceHandlers(registry);
-		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
-		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 	}
 
 	@Bean
